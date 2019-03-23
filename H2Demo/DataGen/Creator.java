6a27130260e3b9257d323a7fa5211ef31177beae
Pardeep Singh <pxs5818@rit.edu>
2019-03-23 16:15:50 -0400

    more DataGen Files

--- H2Demo/DataGen/Creator.java
+++ H2Demo/DataGen/Creator.java
@@ -1,3 +1,6 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 public class Creator {
     public static void main(String[] args) {
         CustomerGen customer = new CustomerGen();
@@ -24,5 +27,33 @@ public class Creator {
         PackageTransportationGen packageTransportation = new PackageTransportationGen(packages, transportation);
         packageTransportation.init();
 
+        customerCSV(customer);
     }
+
+    private static void customerCSV(CustomerGen customer) {
+        String output = "";
+        for(int i = 0; i < 500; i++) {
+            output += customer.fname.get(i) + ",";
+            output += customer.lname.get(i) + ",";
+            output += customer.customerID.get(i) + ",";
+            output += customer.email.get(i) + ",";
+            output += customer.streetNumber.get(i) + ",";
+            output += customer.streetName.get(i) + ",";
+            output += customer.apptNum.get(i) + ",";
+            output += customer.city.get(i) + ",";
+            output += customer.state.get(i) + ",";
+            output += customer.country.get(i) + ",";
+            output += customer.zip.get(i) + "\n";
+        }
+        PrintWriter writer = null;
+        try {
+            writer = new PrintWriter("Customer.csv");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        writer.print(output);
+        writer.close();
+
+    }
+
 }
