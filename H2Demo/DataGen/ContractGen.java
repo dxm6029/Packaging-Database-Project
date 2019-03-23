510b008f55ee7583472335fab676bd59992e5837
dominique-molee <dxm6029@rit.edu>
2019-03-23 16:52:31 -0400

    ContractGen Completed

--- H2Demo/DataGen/ContractGen.java
+++ H2Demo/DataGen/ContractGen.java
@@ -1,2 +1,40 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class ContractGen {
+
+    ArrayList<Integer> paymentID;
+    ArrayList <String> billDate;
+    ArrayList <Integer> totalPackageNum;
+
+    PaymentGen payment;
+
+    public ContractGen(PaymentGen payment){
+        this.payment = payment;
+
+    }
+
+    public void init(){
+        ArrayList<String> paymentTypesList = payment.type;
+        ArrayList<Integer> payIDList = payment.paymentID;
+        fillPaymentID(paymentTypesList, payIDList);
+    }
+
+    public void fillPaymentID(ArrayList<String> paymentTypeList, ArrayList<Integer> payIDList){
+        Random rand = new Random();
+        for (int i = 0; i < 750; i++){
+            if (paymentTypeList.get(i).equals("Contract")){
+                int id = payIDList.get(i);
+                int count = 0;
+                for (int j = 0; j < 750; j++){
+                    if (payIDList.get(i) == id && paymentTypeList.get(i).equals("Contract")){
+                        count++;
+                    }
+                }
+                paymentID.add(id);
+                totalPackageNum.add(count);
+                billDate.add(rand.nextInt(12)+1 + "/1");
+            }
+        }
+    }
 }
--- H2Demo/src/Contract.java
+++ H2Demo/src/Contract.java
@@ -1,22 +1,19 @@
 public class Contract{
 
     int paymentID;
-    double total; // I can also change to float?
     int billDate;
     int totalPackageNum;
 
-    public Contract(int payID, double total, int billDate, int totalPackageNum){
+    public Contract(int payID, int billDate, int totalPackageNum){
         this.paymentID = payID;
-        this.total = total;
         this.billDate = billDate;
         this.totalPackageNum = totalPackageNum;
     }
 
     public Contract(String[] data){
         this.paymentID = Integer.parseInt(data[0]);
-        this.total = Double.parseDouble(data[1]);
-        this.billDate = Integer.parseInt(data[2]);
-        this.totalPackageNum = Integer.parseInt(data[3]);
+        this.billDate = Integer.parseInt(data[1]);
+        this.totalPackageNum = Integer.parseInt(data[2]);
     }
 
     public int getPaymentID() {
@@ -25,14 +22,6 @@ public class Contract{
 
     public void setPaymentID(int paymentID) {
         this.paymentID = paymentID;
-    }
-
-    public double getTotal() {
-        return total;
-    }
-
-    public void setTotal(double total) {
-        this.total = total;
     }
 
     public int getBillDate() {
